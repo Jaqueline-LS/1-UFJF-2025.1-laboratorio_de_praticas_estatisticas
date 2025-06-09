8ddd583335b5067f68d3ca85e78a1377dfeab36f
Jaqueline-LS <jaquelinelamas@ice.ufjf.br>
2025-06-09 11:24:35 -0300

    Análise 1: Pacientes vs. Controles
    Nunhum grupo alimentar apresentou modelo significativo em termos da soma das porções ou das adequações. A hípotese das pesquisadoras não foi significativa

--- Analise1_controles_vs_pacientes.R
+++ Analise1_controles_vs_pacientes.R
@@ -13,25 +13,67 @@ attach(analise1.new)
 
 View(analise1.new)
 
-Y<-soma_porcoes_dia_verduras_legumes
-boxplot(Y~epilepsia)
-boxplot(Y~sexo)
-plot(Y~idade_atual_anos)
 
-ind<-which(epilepsia=="E")
-X<-soma_porcoes_dia_verduras_legumes[ind]
-Y<-soma_porcoes_dia_verduras_legumes[-ind]
-teste.mood.p<-mood.test(X,Y)$p.value
-teste.wilcox.p<-(wilcox.test(X,Y)$p.value) # n e m são menores que 20. Ver o valor tabelado.
-teste.ansari<-ansari.test(X,Y)$p.value
+soma.E.vs.C<-function(Y)
+{
+  boxplot(Y~epilepsia)
+  boxplot(Y~sexo)
+  plot(Y~idade_atual_anos)
+  
+  ind<-which(epilepsia=="E")
+  X1<-Y[ind]
+  X2<-Y[-ind]
+  cat("Mediana pacientes: ", median(X1), "\nMediana controles: ", median(X2),"\n")
+  teste.mood.p<-mood.test(X1,X2)$p.value
+  teste.wilcox.p<-(wilcox.test(X1,X2)$p.value) # n e m são menores que 20. Ver o valor tabelado.
+  teste.ansari<-ansari.test(X1,X2)$p.value
+  cat("Teste Wood: ", teste.mood.p, "\nTeste U: ", teste.wilcox.p)
+  
+  fit1<-lm(Y~sexo+epilepsia+idade_atual_anos, data = analise1.new)
+  summary(fit1)
+  
+}
+soma.E.vs.C(Y=soma_porcoes_dia_laticinios)
+soma.E.vs.C(Y=soma_porcoes_dia_cereais_total)
+soma.E.vs.C(Y=soma_porcoes_dia_cereais_saudaveis)
+soma.E.vs.C(Y=soma_porcoes_dia_verduras_legumes)
+soma.E.vs.C(Y=soma_porcoes_dia_frutas)
+soma.E.vs.C(Y=soma_porcoes_dia_carnes_ovos)
+soma.E.vs.C(Y=soma_porcoes_semana_embutidos)
+soma.E.vs.C(Y=soma_porcoes_semana_salgados_preparacoes)
 
-ind<-which(sexo=="F")
-X<-soma_porcoes_dia_verduras_legumes[ind]
-Y<-soma_porcoes_dia_verduras_legumes[-ind]
-teste.mood.p<-mood.test(X,Y)$p.value
-teste.wilcox.p<-(wilcox.test(X,Y)$p.value) # n e m são menores que 20. Ver o valor tabelado.
-teste.ansari<-ansari.test(X,Y)$p.value
-
-Y<-soma_porcoes_dia_verduras_legumes
-fit1<-lm(Y~sexo+epilepsia, data = analise1.new)
+soma.E.vs.C(Y=soma_porcoes_semana_doces_salgadinhos_guloseimas)
+fit1<-lm(Y~epilepsia, data = analise1.new)
 summary(fit1)
+
+
+Y=soma_porcoes_semana_salgados_preparacoes
+fit1<-lm(Y~epilepsia+idade_atual_anos, data = analise1.new)
+summary(fit1)
+
+
+
+#------------------Regressão Logística---------------
+
+adequacao.E.vs.C<-function(Y)
+{
+  mosaicplot(Y~epilepsia)
+  mosaicplot(Y~sexo)
+  boxplot(idade_atual_anos~Y)
+  if(sum(table(epilepsia, Y)>=5)<4)
+  {
+    Teste<-fisher.test(table(epilepsia, Y))
+  }else{
+    Teste<-chisq.test(table(epilepsia, Y), correct = F)
+    
+  }
+  Teste
+  fit<-glm(Y~sexo+epilepsia+idade_atual_anos, data = analise1.new, family = binomial(link = "logit"))
+  summary(fit)
+  fit<-glm(Y~sexo+epilepsia, data = analise1.new, family = binomial(link = "logit"))
+  summary(fit)
+  
+  
+}
+
+adequacao.E.vs.C(Y=adeq_porcoes_dia_verduras_legumes)
